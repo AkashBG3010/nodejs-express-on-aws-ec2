bb62364eae5442499d6e62ecc563a83296dba2f1
Akash B G <akashbpura@gmail.com>
2022-03-15 22:43:44 +0530

    upadted content

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Connection Success</h4> <p>Version 1.1</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
-      productId: '101',
-      price: 100
+      productId: '01',
+      productName: 'iphone 13',
+      price: 100000
     },
     {
-      productId: '102',
-      price: 150
+      productId: '02',
+      productName: 'Nokia 1200',
+      price: 3000
     }
   ])
 })
